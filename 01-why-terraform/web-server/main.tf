9b033133468766f6ee96222f764e240bee00863a
Takashi Sugimoto <tasugim@amazon.co.jp>
2021-04-29 17:46:12 +0900

    Chap1, 2, 3 finished

---
+++ 01-why-terraform/web-server/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 0.15"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
+  ami               = "ami-0c55b159cbfafe1f0"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo service apache2 start
+    EOF
+}
